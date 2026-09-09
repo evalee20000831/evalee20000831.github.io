352cd6c249ee3b38f889c2c667752fc8c1a7317b
evalee20000831 <evalee20000831@gmail.com>
2026-09-08 21:02:35 -0400

    Change base url back to root

--- room-portfolio/vite.config.js
+++ room-portfolio/vite.config.js
@@ -3,7 +3,7 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
 export default defineConfig({
-  base: '/room-portfolio/', 
+  base: '/', 
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
